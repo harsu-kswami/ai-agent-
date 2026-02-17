94bade5c0deb6e3eaa112006cb07cd7192f83830
harsu-kswami <229167304+harsu-kswami@users.noreply.github.com>
2026-02-17 17:24:37 +0530

    genreator is completed

--- src/agents/complete_python.py
+++ src/agents/complete_python.py
@@ -81,9 +81,63 @@
 
 # features := 
 
-def calculate_discount(cups, price_per_cup):
-    total = cups * price_per_cup
-    return total
-print(calculate_discount(3, 5))
+# def calculate_discount(cups, price_per_cup):
+#     total = cups * price_per_cup
+#     return total
+# print(calculate_discount(3, 5))
 
-# scope
+# # scope
+# def serve_chai():
+#     chai_type = "masala " #local variable, only accessible within the function
+#     print(f"Serving {chai_type} chai.")
+
+# chai_type = "ginger" # global variable, accessible throughout the program
+# print(f"Global chai type: {chai_type}")
+# serve_chai()
+
+
+
+# list compressions :- single line of code 
+
+# list :- [expression for item in iterable if condition]
+
+# menu = [
+#     "masala chai",
+#     "ginger chai",
+#     "lemon tea",
+#     "green tea",
+# ]
+
+# chai_items = [item for item in menu if "tea" in item]
+# print(chai_items)
+
+
+# set :- {expression for item in iterable if condition} only unique values 
+
+# dicitionary :- {key_expression: value_expression for item in iterable if condition}
+
+tea_prices_inr = {
+    "masala chai": 50,
+    "ginger chai": 40,
+    "lemon tea": 30,
+    "green tea": 20,
+}
+
+tea_prices_usd = {tea: price / 80 for tea, price in tea_prices_inr.items()}
+print(tea_prices_usd)
+
+
+# genreator :- (exoressuib for item in iterable if condition) similar to list but it generates items one at a time, which can be more memory efficient for large datasets.
+daily_sales = [100, 150, 200, 250, 300]
+average_sales = sum(sale for sale in daily_sales)
+
+# yield :- use to store and return a value from a generator function, allowing the function to be paused and resumed, which is useful for creating iterators.
+
+def serve_chai():
+    yield "cup 1:masala chai"
+    yield "cup 2:ginger chai"
+    yield "cup 3:lemon tea"
+
+stall  = serve_chai()
+print(next(stall))  # Output: cup 1:masala chai
+print(next(stall))  # Output: cup 2:ginger chai 
